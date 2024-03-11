4fbe7ed0c7f5a5810fd3c5f023b82883622118d3
nayemislamzr <nayemislamzr@gmail.com>
2024-03-11 21:26:59 +0600

    certification system

---
+++ app/(course)/courses/[courseId]/grades/certificate/page.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Certificate from './_components/Certificate';
+
+const data = {
+  logo: 'https://seeklogo.com/images/S/shahjalal-university-of-science-and-technology-logo-F34BC01F81-seeklogo.com.png',
+  name: 'Nayem Islam',
+  course: 'Data Structure and Algorithm using JavaScript',
+  dateOfConductStart: '2020-05-20',
+  dateOfConductEnd: '2023-05-20',
+  signature: 'https://a.storyblok.com/f/191576/1200x800/b7ad4902a2/signature_maker_after_.webp',
+  signatureDetails: 'CEO, CipherGuy',
+}
+
+const CertificateGenerator = () => {
+  const { logo, name, course, dateOfConductStart, dateOfConductEnd, signature, signatureDetails } = data;
+
+  return (
+    <Certificate
+      logo={logo}
+      name={name}
+      course={course}
+      dateOfConductStart={dateOfConductStart}
+      dateOfConductEnd={dateOfConductEnd}
+      signature={signature}
+      signatureDetails={signatureDetails}
+    />
+  )
+}
+
+export default CertificateGenerator
